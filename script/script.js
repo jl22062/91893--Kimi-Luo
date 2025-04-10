fa8d6406e48da37475e0d9e5ac3337cedc96b7fa
Kimi <jl22062@my.westlake.school.nz>
2025-04-11 10:14:18 +1200

    .

--- script/script.js
+++
@@ -1,8 +0,0 @@
-const inputBox = document.querySelector('.searchInput');
-const renderBox = document.getElementById('latexRender');
-
-inputBox.addEventListener('input', () => {
-const latex = inputBox.value;
-renderBox.innerHTML = `\\(${latex}\\)`;
-MathJax.typeset(); // Re-render LaTeX
-});
